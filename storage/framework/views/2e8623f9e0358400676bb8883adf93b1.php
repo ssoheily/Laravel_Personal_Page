ace17b69931a3a4261d5b2285d710a96f1deae33
Saman <idk@idk.com>
2023-07-15 19:19:40 +0200

    first commit

---
+++ storage/framework/views/2e8623f9e0358400676bb8883adf93b1.php
@@ -0,0 +1,2 @@
+<h4>about-page</h4>
+<?php /**PATH /home/parallels/lv-101/resources/views/about.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b6010239fdd3c6b9426a259304d3c78f.php
@@ -0,0 +1,22 @@
+<div class="home">
+    <div class="container-fluid">
+        <div class="row min-vh-100 align-items-center">
+            <div class="col-lg-6">
+                <div class="home-text">
+                    <h1 class="fw-bold pb-3">Hi, welcome to my website</h1>
+                    <h2 class="fw-bold">I am Saman Soheilyfar</h2>
+                    <h4>Web designer and programmer</h4>
+                    <p class="text-muted">My analytical, structured, and goal-oriented approach to work,
+                        coupled with my strong interest in complex software solutions, complement my profile.
+                        These skills have consistently contributed to my successful academic and professional achievements.
+                        I am highly motivated, possess a quick grasp of concepts, and exhibit flexibility in adapting to new requirements.
+                        Furthermore, my ability to collaborate effectively across different domains underscores my aptitude for teamwork.</p>
+                    <a href="" class="btn btn-danger px-5">About me</a>
+                </div>
+            </div>
+            <div class="col-lg-6 home-img min-vh-100"></div>
+
+        </div>
+    </div>
+</div>
+<?php /**PATH /home/parallels/lv-101/resources/views/front/home.blade.php ENDPATH**/ ?>
