17558aa4f1b43f73b04e30fc5e9a4fb6ec437a20
Saman <idk@idk.com>
2023-07-16 03:02:12 +0200

    fixed untracked files

--- storage/framework/views/5da945c45d7c56b112f4dc5472cba56f.php
+++
@@ -1,5 +0,0 @@
-
-<footer class="bg-dark py-5 mt-5">
-    <p class="m-0 text-white text-center">  Designed by Sam :|</p>
-</footer>
-<?php /**PATH /home/parallels/lv-101/resources/views/front/footer.blade.php ENDPATH**/ ?>
